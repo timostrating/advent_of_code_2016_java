62a2cab31564c3f894ecebbfb452a1ce9b108a89
Timo Strating <Timostrating@home.nl>
2016-12-17 21:48:40 +0100

    day 3 part1

---
+++ src/day3/part1.java
@@ -0,0 +1,52 @@
+package day3;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Sneeuwpopsneeuw on 15-Dec-16.
+ */
+public class part1 {
+    public static void main(String[] args) throws IOException{ new part1().start(); }
+    public void start() throws IOException {
+        File inFile = new File ("src/day3/input.txt");
+        Scanner sc = new Scanner (inFile);
+        triangle tri = new triangle(5, 10, 25);
+        int output = 0;
+
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+
+            Matcher m = Pattern.compile("\\d+").matcher(line);
+
+            m.find();       // <-- this looks ugly I know
+                tri.a = Integer.parseInt( m.group() );
+            m.find();
+                tri.b = Integer.parseInt( m.group() );
+            m.find();
+                tri.c = Integer.parseInt( m.group() );
+
+            System.out.println( tri.a+" : "+tri.b+" : "+tri.c );
+            output += (tri.isValid())? +1 : +0;
+        }
+
+        System.out.println(" ans = "+output);
+    }
+
+    class triangle {
+        public int a, b, c;
+
+        public triangle(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public boolean isValid() {
+            return (a+b > c && b+c > a && c+a > b);
+        }
+    }
+}
